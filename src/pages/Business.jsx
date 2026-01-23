24fb3a270bd2ee29bb2d15b16e19ab2c2d3e454a
Ashot <ashot.muradyan.ait@gmail.com>
2026-01-23 19:08:09 +0400

    creat div for Business

--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -1,10 +1,180 @@
 import React from "react";
-
+import "./Business.css"
 function Business() {
   return (
     <section>
-      <h2>Business Page</h2>
-      <p>Welcome to the business page!</p>
+       <div className="div2">
+    </div>
+   <div className="div2">
+    <img id="img1" src="https://www.ucom.am/images/main_logo.svg" alt="" />
+    <p id="pteg">Շարժական կապ</p>
+    <br />
+    <br />
+    <p id="p2">ֆիքսված ծառայություններ</p>
+    <br />
+    <br />
+    <p id="p3">Բիզնես լուծումներ</p>
+    <br />
+    <br />
+    <p id="p4">Ամպային լուծումներ</p>
+    <br />
+    <br />
+    <p id="p">Uplay</p>
+    <i className="fa fa-search" aria-hidden="true" />
+    <br />
+    <br />
+    <i className="fa fa-shopping-cart" aria-hidden="true" />
+    <p id="pt">Առցանց խանութ</p>
+    <br />
+    <br />
+    <i className="fa fa-credit-card" aria-hidden="true" />
+    <p id="pv">Վճարել</p>
+  </div>
+  <div>
+    <img
+      id="img2"
+      src="https://www.ucom.am/storage/files/digital-banner-screens-02.png-786x_-quality(75)-webp(80)-o(png).webp?token=70a9103d4c5209ac5cc673e6d30a9c4d"
+      alt=""
+    />
+  </div>
+  <br />
+  <br />
+  <div className="dvushik">
+    <img
+      id="is1"
+      src="https://www.ucom.am/storage/files/person-with-phone.png-_x622-quality(75)-webp(80)-o(png).webp?token=19b0c955f60cde1ca934a781195a42f1"
+      alt=""
+    />
+    <img
+      id="is2"
+      src="https://www.ucom.am/storage/files/11111.png-_x622-quality(75)-webp(80)-o(png).webp?token=8fe590debbb45e910f3b68216ee0c8cf"
+      alt=""
+    />
+    <img
+      id="is3"
+      src="https://www.ucom.am/storage/files/voffice_1.png-_x622-quality(75)-webp(80)-o(png).webp?token=295ae89bcccb16a742fd35455af19cea"
+      alt=""
+    />
+    <img
+      id="is4"
+      src="https://www.ucom.am/storage/files/vpc1320x770-15.png-_x622-quality(75)-webp(80)-o(png).webp?token=c5dfd7adfc0c3b2597dff432909a2f78"
+      alt=""
+    />
+    <img
+      id="is5"
+      src="https://www.ucom.am/storage/files/news-arm.jpg-_x622-quality(75)-webp(80)-o(jpg).webp?token=2f5452caa497dcf8fe99b71c33cc16e0"
+      alt=""
+    />
+  </div>
+  <div>
+    <p id="pr">Լավագույն առաջարկ</p>
+  </div>
+  <div className="amboxj">
+    <div className="divik">
+      <div className="divik2">
+        <p id="pto">BUSINESS Basic</p>
+        <i id="k1" className="fa fa-globe" aria-hidden="true" />
+        <p>20 ԳԲ ինտերնետ</p>
+        <img src="https://www.ucom.am/storage/files/instagram.svg" alt="" />
+        <img src="https://www.ucom.am/storage/files/youtube.svg" alt="" />
+        <img
+          id="im"
+          src="https://www.ucom.am/storage/files/yandex-taxi.svg"
+          alt=""
+        />
+        <img
+          id="im2"
+          src="https://www.ucom.am/storage/files/viber.svg"
+          alt=""
+        />
+        <img id="im3" src="https://www.ucom.am/storage/files/gg.svg" alt="" />
+        <img
+          id="im4"
+          src="https://www.ucom.am/storage/files/duolingo.svg"
+          alt=""
+        />
+      </div>
+      <i id="k2" className="fa fa-volume-control-phone" aria-hidden="true" />
+      <p>1000 րոպե</p>
+      <img
+        id="ig1"
+        src="https://www.ucom.am/storage/files/flag-01.svg"
+        alt=""
+      />
+      <img
+        id="ig2"
+        src="https://www.ucom.am/storage/files/flag-03.svg"
+        alt=""
+      />
+      <img
+        id="ig3"
+        src="https://www.ucom.am/storage/files/flag-for-armenia-svgrepo-com.svg"
+        alt=""
+      />
+      <img
+        id="ig4"
+        src="https://www.ucom.am/storage/files/ca-circle-01.png-64x_-quality(75)-webp(80)-o(png).webp?token=fde3fb0e5e63c798e892358d6f79fb87"
+        alt=""
+      />
+      <i className="fa fa-commenting-o" aria-hidden="true" />
+      <p>1000 SMS</p>
+      <br />
+      <br />
+      <p id="ptego">2800֏ Ավելին</p>
+      <p id="dvo">Միանալ</p>
+    </div>
+    <div className="cros2">
+      <p id="pto2">BUSINESS Optimum</p>
+      <i id="a3" className="fa fa-globe" aria-hidden="true" />
+      <p>Անսահմանափակ ինտերնետ</p>
+      <i id="a2" className="fa fa-volume-control-phone" aria-hidden="true" />
+      <p>3000 րոպե</p>
+      <img src="https://www.ucom.am/storage/files/instagram.svg" alt="" />
+      <img src="https://www.ucom.am/storage/files/youtube.svg" alt="" />
+      <img
+        id="imgh"
+        src="https://www.ucom.am/storage/files/yandex-taxi.svg"
+        alt=""
+      />
+      <img
+        id="imgh2"
+        src="https://www.ucom.am/storage/files/viber.svg"
+        alt=""
+      />
+      <img id="imgh3" src="https://www.ucom.am/storage/files/gg.svg" alt="" />
+      <img
+        id="imgh4"
+        src="https://www.ucom.am/storage/files/duolingo.svg"
+        alt=""
+      />
+      <br />
+      <i id="a" className="fa fa-commenting" aria-hidden="true" />
+      <p>3000 SMS</p>
+      <p id="ptego2">5000֏ Ավելին</p>
+      <p id="dvo2">Միանալ</p>
+    </div>
+    <div className="dvolo">
+      <p id="pto4">uNet Corporate Basic</p>
+      <p>35 Մբ/վ արագություն</p>
+      <i class="fa fa-wifi" aria-hidden="true"></i>
+      <p>Wi-Fi սարք</p>
+      <i id="ha" className="fa fa-globe" aria-hidden="true" />
+      <p>FTTH տեխնոլոգիա</p>
+      <p id="ptego2">18000֏ Ավելին</p>
+      <p id="dvo3">Միանալ</p>
+    </div>
+    <div className="Hayk">
+      <p id="pto5">uNet Corporate Premium</p>
+      <i id="sa4" className="fa fa-wifi" aria-hidden="true" />
+      <p>75Մբ/վ արագություն</p>
+      <i id="sa3" className="fa fa-wifi" aria-hidden="true" />
+      <p>Wi-Fi սարք</p>
+      <i class="fa fa-globe" aria-hidden="true"></i>
+      <p>FTTH տեխնոլոգիա</p>
+      <p id="ptego2">40000֏ Ավելին</p>
+      <p id="dvo3">Միանալ</p>
+    </div>
+  </div>
     </section>
   );
 }

9af6a563590499bcac715146db2a26794098ecca
Ashot <ashot.muradyan.ait@gmail.com>
2026-01-27 23:45:49 +0400

    creat divs for Ucom cards

--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -12,8 +12,7 @@ function Business() {
     />
     <div className="flex gap-2 text-sm font-medium text-white items-center">
       <p
-        id="ptego"
-        className="transition-all duration-300 hover:scale-110 hover:bg-[#313231] hover:w-[250px] hover:h-[30px] rounded text-center">
+        className="transition-all text-white duration-300 hover:scale-110 hover:bg-[#313231] hover:w-[250px] hover:h-[30px] rounded text-center">
         Շարժական կապ
       </p>
       <p
@@ -58,7 +57,7 @@ function Business() {
   />
 
   <div className="flex justify-between gap-6 mx-auto">
-    {[ "is1", "is2", "is3", "is4", "is5" ].map((id, i) => (
+    {[ "is1", "is2", "is3", "is4",].map((id, i) => (
       <img
         key={id}
         id={id}
@@ -84,6 +83,7 @@ function Business() {
       <p className="text-white">1000 րոպե</p>
       <p className="text-white">1000 SMS</p>
       <p className="text-white mt-4">2800֏</p>
+      <br /><br />
       <p
         id="dvo"
         className="bg-[#7CE00B] w-[150px] h-[30px] text-center rounded font-bold mx-auto mt-4 transition hover:bg-green-300">
@@ -101,13 +101,64 @@ function Business() {
       <p className="text-white">3000 րոպե</p>
       <p className="text-white">3000 SMS</p>
       <p className="text-white mt-4">5000֏</p>
+      <br /><br />
       <p
         id="dvo2"
         className="bg-[#7CE00B] w-[150px] h-[30px] text-center rounded font-bold mx-auto mt-4 transition hover:bg-green-300">
         Միանալ
       </p>
     </div>
-
+        <div className="border border-gray-500 w-[300px] h-[350px] rounded-lg">
+      <p
+        id="pto2"
+        className="bg-[#71c313] text-white text-center h-[50px] rounded-lg text-lg flex items-center justify-center">
+        uNet Corporate Basic
+      </p>
+      <p className="text-white mt-4">35Մբ/վ արագություն</p>
+      <p className="text-white">Wi-Fi սարք</p>
+      <p className="text-white">FTTH տեխնոլոգիա</p>
+      <p className="text-white mt-4">18000֏</p>
+      <br /><br />
+      <p
+        id="dvo2"
+        className="bg-[#7CE00B] w-[150px] h-[30px] text-center rounded font-bold mx-auto mt-4 transition hover:bg-green-300">
+        Միանալ
+      </p>
+    </div>
+       <div className="border border-gray-500 w-[300px] h-[350px] rounded-lg">
+      <p
+        id="pto2"
+        className="bg-[#71c313] text-white text-center h-[50px] rounded-lg text-lg flex items-center justify-center">
+        uNet Corporate Premium
+      </p>
+      <p className="text-white mt-4">75Մբ/վ արագություն</p>
+      <p className="text-white">Wi-Fi սարք</p>
+      <p className="text-white">FTTH տեխնոլոգիա</p>
+      <p className="text-white mt-4">40000֏</p>
+      <br /><br />
+      <p
+        className="bg-[#7CE00B] w-[150px] h-[30px] text-center rounded font-bold mx-auto mt-4 transition hover:bg-green-300">
+        Միանալ
+      </p>
+    </div>
+       <div className="border border-gray-500 w-[300px] h-[350px] rounded-lg">
+      <p
+        id="pto2"
+        className="bg-[#71c313] text-white text-center h-[50px] rounded-lg text-lg flex items-center justify-center">
+        Uplay Business Extra
+      </p>
+      <p className="text-white mt-4">165+ հեռուստաալիք՝</p>
+      <p className="text-green mt-4">Uplay հարթակում</p>
+      <p className="text-white">34 HD ալիք</p>
+      <p className="text-white">7 օր Catch-up</p>
+      <p className="text-white">Անվճար Smart TV Box</p>
+      <p className="text-white mt-4">4900֏</p>
+      <br /><br />
+      <p
+        className="bg-[#7CE00B] w-[150px] h-[30px] text-center rounded font-bold mx-auto mt-4 transition hover:bg-green-300">
+        Միանալ
+      </p>
+    </div>
   </div>
 </section>
 

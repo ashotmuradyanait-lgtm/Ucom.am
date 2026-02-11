c7ddd61080469871f000fc0b22cf3a32d14ce5c8
Ashot <ashot.muradyan.ait@gmail.com>
2026-02-11 16:32:47 +0400

    add information

--- src/dtb.js
+++ src/dtb.js
@@ -1,11 +1,54 @@
 const dataBase = {
-    cards: [
-        {
-            id: 1, name:"Level Up+ 3000",
-            "div":{
-                "img": 11,
-                "icon": "fa-fa icon",
-            }
-        }
-    ]
-}
+  cards: [
+    {
+      id: 1,
+      title: "Level Up+ 3000",
+      description: "Ամսական ինտերնետ փաթեթ 3000 դրամով",
+      price: 3000,
+      currency: "AMD",
+      isActive: true,
+      category: "Internet",
+      image: "/images/levelup3000.png",
+      icon: "fa-solid fa-wifi",
+      features: [
+        "Անսահմանափակ ինտերնետ",
+        "5G աջակցություն",
+        "Անվճար SIM քարտ"
+      ]
+    },
+    {
+      id: 2,
+      title: "Level Up+ 5000",
+      description: "Ավելի արագ և մեծ ծավալով փաթեթ",
+      price: 5000,
+      currency: "AMD",
+      isActive: true,
+      category: "Internet",
+      image: "/images/levelup5000.png",
+      icon: "fa-solid fa-bolt",
+      features: [
+        "Ավելի բարձր արագություն",
+        "Priority support",
+        "Անսահմանափակ զանգեր"
+      ]
+    },
+    {
+      id: 3,
+      title: "Business Pro",
+      description: "Բիզնես լուծում մեծ թիմերի համար",
+      price: 12000,
+      currency: "AMD",
+      isActive: false,
+      category: "Business",
+      image: "/images/businesspro.png",
+      icon: "fa-solid fa-briefcase",
+      features: [
+        "Մինչև 10 աշխատակից",
+        "Dedicated manager",
+        "Cloud storage"
+      ]
+    }
+  ]
+};
+
+export default dataBase;
